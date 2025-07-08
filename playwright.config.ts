616e98dc2a7087bfb5e56140798461672ccaae93
conservationtimothy <ogbemu.diatimothy@gmail.com>
2025-07-08 12:46:58 +0100

    headless shpuld be true

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   use: {
     baseURL: "http://localhost:8080",
-    headless: false,
+    headless: true,
   },
   workers: process.env.CI ? 1 : undefined,
 });
