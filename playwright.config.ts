3199b65b1618a2142be81ea176b090e393b64ba8
conservationtimothy <ogbemu.diatimothy@gmail.com>
2025-07-09 07:48:13 +0100

    use local

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     : {
         webServer: {
           command: "pnpm dev",
-          url: "http://127.0.0.1:8080",
+          url: "http://localhost:8080",
           timeout: 200_000,
           reuseExistingServer: false,
           stdout: "pipe",
@@ -23,7 +23,7 @@ export default defineConfig({
         },
       }),
   use: {
-    baseURL: "http://127.0.0.1:8080",
+    baseURL: "http://localhost:8080",
     headless: true,
   },
   workers: process.env.CI ? 1 : undefined,
