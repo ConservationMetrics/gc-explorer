a29f7524860efe865e46c795db5a9d315fe9839e
conservationtimothy <ogbemu.diatimothy@gmail.com>
2026-01-26 15:00:30 +0100

    feat: use Auth0 user name for incident created_by field

--- server/api/auth/auth0.get.ts
+++ server/api/auth/auth0.get.ts
@@ -391,6 +391,9 @@ export default oauthAuth0EventHandler({
           auth0: user.email,
           roles: userRoles,
           userRole,
+          // Store name/nickname from auth0 user if available
+          name: (user.name as string) || undefined,
+          nickname: (user.nickname as string) || undefined,
         },
         loggedInAt: Date.now(),
       });
