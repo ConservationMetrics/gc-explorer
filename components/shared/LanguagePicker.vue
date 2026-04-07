453d3f369ce2402ff6acdd61c02b5009c5315e07
Rudo Kemper <rtakemper@gmail.com>
2026-04-06 21:44:16 -0400

    Remove orphaned LanguagePicker

--- components/shared/LanguagePicker.vue
+++
@@ -1,7 +0,0 @@
-<!--
-  DEPRECATED: This component is no longer used.
-  We now use GlobeLanguagePicker instead.
-  
-  For reference, see the last working implementation at:
-  SHA: 66f561dcd7e090877bbce485ff41f0927ebc39db
--->
