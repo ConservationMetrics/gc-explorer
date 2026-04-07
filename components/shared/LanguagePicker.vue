80f74e3a6094ba3a065dcab7fd8320ac86d2286c
Rudo Kemper <rtakemper@gmail.com>
2026-04-06 21:31:07 -0400

    Restore LanguagePicker (delete is out of scope)

---
+++ components/shared/LanguagePicker.vue
@@ -0,0 +1,7 @@
+<!--
+  DEPRECATED: This component is no longer used.
+  We now use GlobeLanguagePicker instead.
+  
+  For reference, see the last working implementation at:
+  SHA: 66f561dcd7e090877bbce485ff41f0927ebc39db
+-->
