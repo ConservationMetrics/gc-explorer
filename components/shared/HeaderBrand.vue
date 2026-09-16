4e87e150f383440162f921a811ad51aa6bf030df
Rudo Kemper <rtakemper@gmail.com>
2026-09-16 16:14:25 -0400

    AppHeader abbreviates GC for small viewports

--- components/shared/HeaderBrand.vue
+++ components/shared/HeaderBrand.vue
@@ -16,7 +16,8 @@ const logoSrc = withBase("/gcexplorer.png", useRuntimeConfig().app.baseURL);
     />
     <div class="min-w-0 rounded-lg py-2 pl-0 pr-1 min-[1001px]:pr-2">
       <h1 class="m-0 text-lg font-bold leading-tight whitespace-nowrap">
-        Guardian Connector Explorer
+        <span class="max-[1000px]:hidden">Guardian Connector Explorer</span>
+        <span class="hidden max-[1000px]:inline">GC Explorer</span>
       </h1>
     </div>
   </NuxtLink>
